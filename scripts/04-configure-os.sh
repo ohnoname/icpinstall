bd6946c9a80b4bf2df5276ca7e59f964c2781ba4
Bas Nieuwenburg <bas_nieuwenburg@hotmail.com>
2018-11-07 17:17:39 +0100

    os configured

--- scripts/04-configure-os.sh
+++ scripts/04-configure-os.sh
@@ -32,3 +32,21 @@ for ((i=0; i < $NUM_WORKERS; i++)); do
   ssh ${SSH_USER}@${WORKER_HOSTNAMES[i]} sudo service docker start
 
 done
+
+for ((i=0; i < $NUM_PROXYS; i++)); do
+  # Disable SELinux
+  ssh ${SSH_USER}@${PROXY_HOSTNAMES[i]} sudo setenforce 0
+
+  # Set VM max map count
+  ssh ${SSH_USER}@${PROXY_HOSTNAMES[i]} sudo sysctl -w vm.max_map_count=262144
+  ssh ${SSH_USER}@${PROXY_HOSTNAMES[i]} "echo vm.max_map_count=262144 | sudo tee -a /etc/sysctl.conf"
+
+  # Sync time
+  ssh ${SSH_USER}@${PROXY_HOSTNAMES[i]} sudo ntpdate -s time.nist.gov
+
+  # Start docker
+  ssh ${SSH_USER}@${PROXY_HOSTNAMES[i]} sudo service docker start
+
+done
+
+

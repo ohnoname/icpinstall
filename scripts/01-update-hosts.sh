13f7613d2e98055862ddcc8113bb0ca6b2b5db8a
Bas Nieuwenburg <bas_nieuwenburg@hotmail.com>
2018-11-07 16:48:34 +0100

    aanpassing proxy

--- scripts/01-update-hosts.sh
+++ scripts/01-update-hosts.sh
@@ -17,6 +17,8 @@ echo "ff02::2 ip6-allrouters" | sudo tee -a /etc/hosts
 echo "ff02::3 ip6-allhosts" | sudo tee -a /etc/hosts
 echo "" | sudo tee -a /etc/hosts
 
+echo "$MASTER_IP icpmaster" | sudo tee -a /etc/hosts
+echo "$PROXY_IP icpproxy" | sudo tee -a /etc/hosts
 # Loop through the array
 for ((i=0; i < $NUM_WORKERS; i++)); do
   echo "${WORKER_IPS[i]} ${WORKER_HOSTNAMES[i]}" | sudo tee -a /etc/hosts
@@ -26,7 +28,8 @@ echo "" | sudo tee -a /etc/hosts
 sudo cp /etc/hosts ~/worker-hosts
 sudo chown $USER ~/worker-hosts
 
-echo "$MASTER_IP mycluster.icp" | sudo tee -a /etc/hosts
+
+
 
 for ((i=0; i < $NUM_WORKERS; i++)); do
   # Remove old instance of host

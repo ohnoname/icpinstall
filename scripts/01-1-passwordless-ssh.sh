861d4f26354850639e9e796b7f6ddff2435c861b
Bas Nieuwenburg <bas_nieuwenburg@hotmail.com>
2018-11-08 12:18:13 +0100

    fix

--- scripts/01-1-passwordless-ssh.sh
+++ scripts/01-1-passwordless-ssh.sh
@@ -6,19 +6,6 @@ source ./00-variables.sh
 # Generate RSA key
 ssh-keygen -b 4096 -t rsa -f ~/.ssh/id_rsa -N ""
 
-# Move RSA key to masters
-for ((i=0; i < $NUM_MASTERS; i++)); do
-  # Prevent SSH identity prompts
-  # If host IP exists in known hosts remove it
-  ssh-keygen -R ${MASTER_IPS[i]}
-  # Add host IP to known hosts
-  ssh-keyscan -H ${MASTER_IPS[i]} | tee -a ~/.ssh/known_hosts
-  
-  # Copy over key (Will prompt for password)
-  scp ~/.ssh/id_rsa.pub ${SSH_USER}@${MASTER_IPS[i]}:~/id_rsa.pub
-  ssh ${SSH_USER}@${MASTER_IPS[i]} 'mkdir -p ~/.ssh; cat ~/id_rsa.pub | tee -a ~/.ssh/authorized_keys'
-done
-
 # Move RSA key to workers
 for ((i=0; i < $NUM_WORKERS; i++)); do
   # Prevent SSH identity prompts

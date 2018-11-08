16aa9f832385f99b3e145b6fb4a7dfdad02f0900
Bas Nieuwenburg <bas_nieuwenburg@hotmail.com>
2018-11-08 08:49:40 +0100

    final install

--- scripts/07-configure-installer.sh
+++ scripts/07-configure-installer.sh
@@ -7,8 +7,10 @@ source 00-variables.sh
 sudo cp ~/.ssh/master.id_rsa /opt/ibm-cloud-private-3.1.0/cluster/ssh_key
 
 # Configure hosts
-echo "[master]" | sudo tee /opt/ibm-cloud-private-3.1.0/cluster/hosts
-echo "${MASTER_IP}" | sudo tee -a /opt/ibm-cloud-private-3.1.0/cluster/hosts
+echo "[master]" | sudo tee -a /opt/ibm-cloud-private-3.1.0/cluster/hosts
+for ((i=0; i < $NUM_MASTERS; i++)); do
+  echo ${MASTER_IPS[i]} | sudo tee -a /opt/ibm-cloud-private-3.1.0/cluster/hosts
+done
 echo "" | sudo tee -a /opt/ibm-cloud-private-3.1.0/cluster/hosts
 
 echo "[worker]" | sudo tee -a /opt/ibm-cloud-private-3.1.0/cluster/hosts
@@ -18,7 +20,10 @@ done
 echo "" | sudo tee -a /opt/ibm-cloud-private-3.1.0/cluster/hosts
 
 echo "[proxy]" | sudo tee -a /opt/ibm-cloud-private-3.1.0/cluster/hosts
-echo "${PROXY_IP}" | sudo tee -a /opt/ibm-cloud-private-3.1.0/cluster/hosts
+for ((i=0; i < $NUM_PROXYS; i++)); do
+  echo ${PROXY_IPS[i]} | sudo tee -a /opt/ibm-cloud-private-3.1.0/cluster/hosts
+done
+echo "" | sudo tee -a /opt/ibm-cloud-private-3.1.0/cluster/hosts
 
 # Add line for external IP in config
 #echo "cluster_access_ip: ${PUBLIC_IP}" | sudo tee -a /opt/ibm-cloud-private-3.1.0/cluster/config.yaml
--- scripts/09-kubeconfig.sh
+++ scripts/09-kubeconfig.sh
@@ -15,7 +15,7 @@ sudo cp cluster/cfc-certs/kubecfg.* ~/.kube/
 sudo chown -R $USER  ~/.kube/
 
 #Set kube config
-kubectl config set-cluster cfc-cluster --server=https://mycluster.icp:8001 --insecure-skip-tls-verify=true
+kubectl config set-cluster cfc-cluster --server=https://icpmaster1.icp:8001 --insecure-skip-tls-verify=true
 kubectl config set-context kubectl --cluster=cfc-cluster
 kubectl config set-credentials user --client-certificate=$HOME/.kube/kubecfg.crt --client-key=$HOME/.kube/kubecfg.key
 kubectl config set-context kubectl --user=user
